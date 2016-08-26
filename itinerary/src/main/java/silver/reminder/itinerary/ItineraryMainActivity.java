a83f9b6df9a120400a9e4d307b272d41fcb2d8bf
najachen <najachen@gmail.com>
2016-08-26 14:38:02 +0800

    Initial commit

---
+++ itinerary/src/main/java/silver/reminder/itinerary/ItineraryMainActivity.java
@@ -0,0 +1,21 @@
+package silver.reminder.itinerary;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ItineraryMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_itinerary_main);
+
+
+    }
+    /**
+     *  modify test
+     */
+    /**
+     * fgsdgsgs
+     */
+}
