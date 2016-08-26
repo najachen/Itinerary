f416934d83a7c3931f63d10d5c53276837c674fe
hsuan <hsuan9077@gmail.com>
2016-08-26 17:52:01 +0800

    R  FloatingActionButton 有問題

---
+++ itinerary/src/main/java/silver/reminder/itinerary/TaskListActivity.java
@@ -0,0 +1,25 @@
+package silver.reminder.itinerary;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class TaskListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.list_task_activity);
+//        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+//        setSupportActionBar(toolbar);
+
+//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+//        fab.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+//            }
+//        });
+    }
+
+}
