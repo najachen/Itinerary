79c30e45f1380baefa18f2fcca6471cc9270f834
Stanley Cheng <stanleycheng1025@gmail.com>
2016-08-28 22:01:14 +0800

    commit 相同名稱但不同檔案的版控測試

--- itinerary/src/main/java/silver/reminder/itinerary/bo/ItineraryBO.java
+++ itinerary/src/main/java/silver/reminder/itinerary/bo/ItineraryBO.java
@@ -4,13 +4,30 @@ import android.database.Cursor;
 
 import java.util.List;
 
+import silver.reminder.itinerary.javabean.Note;
 import silver.reminder.itinerary.javabean.Task;
-
+import silver.reminder.itinerary.javabean.Shopping;
 /**
- * Created by StanleyCheng on 2016/8/27.
+ * Sun Aug 28 21:57:45 CST 2016 by freemarker template
  */
-public interface ItineraryBO {
+public interface ItineraryBo {
 
+    long createNote(Note note);
+
+    void createNoteList(List<Note> noteList);
+
+    int modifyNote(Note note);
+
+    void modifyNoteList(List<Note> noteList);
+
+    void removeNote(Integer id);
+
+    void removeNoteList(List<Integer> noteIdList);
+
+    Note findNoteById(Integer id);
+
+    Cursor findNoteList(Note note);
+    
     long createTask(Task task);
 
     void createTaskList(List<Task> taskList);
@@ -26,4 +43,21 @@ public interface ItineraryBO {
     Task findTaskById(Integer id);
 
     Cursor findTaskList(Task task);
+    
+    long createShopping(Shopping shopping);
+
+    void createShoppingList(List<Shopping> shoppingList);
+
+    int modifyShopping(Shopping shopping);
+
+    void modifyShoppingList(List<Shopping> shoppingList);
+
+    void removeShopping(Integer id);
+
+    void removeShoppingList(List<Integer> shoppingIdList);
+
+    Shopping findShoppingById(Integer id);
+
+    Cursor findShoppingList(Shopping shopping);
+    
 }
